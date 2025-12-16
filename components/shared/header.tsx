1781bfa637eba17fd2902e53ed946dabc67b689e
Artem Lysuk <artemlysuk36@gmail.com>
2025-12-16 21:52:21 +0200

    fix210

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { Container } from "./container";
-import { FC, useEffect, useState } from "react";
+import {cn} from "@/lib/utils";
+import {Container} from "./container";
+import {FC, Suspense, useEffect, useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { SearchInput } from "./search-input";
-import { CartButton } from "./cart-button";
-import { useSearchParams } from "next/navigation";
+import {SearchInput} from "./search-input";
+import {CartButton} from "./cart-button";
+import {useSearchParams} from "next/navigation";
 import toast from "react-hot-toast";
-import { useSession } from "next-auth/react";
-import { ProfileButton } from "./profile-button";
-import { AuthModal } from "./modals/auth-modal";
-import { useRouter } from "next/navigation";
+import {useSession} from "next-auth/react";
+import {ProfileButton} from "./profile-button";
+import {AuthModal} from "./modals/auth-modal";
+import {useRouter} from "next/navigation";
 
 interface Props {
   hasSearch?: boolean;
@@ -21,15 +21,15 @@ interface Props {
 }
 
 export const Header: FC<Props> = ({
-  className,
-  hasSearch = true,
-  hasCart = true,
-}) => {
+                                    className,
+                                    hasSearch = true,
+                                    hasCart = true,
+                                  }) => {
   const router = useRouter();
   const [openAuthModal, setOpenAuthModal] = useState(false);
 
   const searchParams = useSearchParams();
-  const { data: session } = useSession();
+  const {data: session} = useSession();
 
   useEffect(() => {
     let toastMessage = "";
@@ -52,34 +52,42 @@ export const Header: FC<Props> = ({
 
   return (
     <header className={cn(" border-b", className)}>
-      <Container className="flex items-center justify-between py-8">
-        <Link href="/">
-          <div className="flex items-center gap-4">
-            <Image src="/header/logo.png" alt="logo" width={35} height={35} />
-            <div>
-              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-              <p className="text-sm text-gray-400 leading-3">
-                вкусней уже некуда
-              </p>
+      <Suspense>
+        <Container className="flex items-center justify-between py-8">
+          <Link href="/">
+            <div className="flex items-center gap-4">
+              <Image
+                src="/header/logo.png"
+                alt="logo"
+                width={35}
+                height={35}
+              />
+              <div>
+                <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                <p className="text-sm text-gray-400 leading-3">
+                  вкусней уже некуда
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
 
-        {hasSearch && (
-          <div className="mx-10 flex-1">
-            <SearchInput />
-          </div>
-        )}
+          {hasSearch && (
+            <div className="mx-10 flex-1">
+              <SearchInput />
+            </div>
+          )}
 
-        <div className="flex items-center gap-3">
-          <AuthModal
-            open={openAuthModal}
-            onClose={() => setOpenAuthModal(false)}
-          />
-          <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
-          {hasCart && <CartButton />}
-        </div>
-      </Container>
+          <div className="flex items-center gap-3">
+            <AuthModal
+              open={openAuthModal}
+              onClose={() => setOpenAuthModal(false)}
+            />
+            <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
+            {hasCart && <CartButton />}
+          </div>
+        </Container>
+      </Suspense>
+
     </header>
   );
 };
