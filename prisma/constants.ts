cc31d362fddb91c4fec34030293730b3d8e20a87
Artem Lysuk <artemlysuk36@gmail.com>
2025-12-20 14:37:00 +0200

    commit before update prisma

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -21,67 +21,67 @@ export const ingredients = [
     name: 'Сырный бортик',
     price: 179,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png',
+      'assets/images/dataItems/ingredients/cheeseBortik.png',
   },
   {
     name: 'Сливочная моцарелла',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png',
+      'assets/images/dataItems/ingredients/macarella.png',
   },
   {
     name: 'Сыры чеддер и пармезан',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796',
+      'assets/images/dataItems/ingredients/chedder-parmezan.png',
   },
   {
     name: 'Острый перец халапеньо',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png',
+      'assets/images/dataItems/ingredients/hot-halapenio.png',
   },
   {
     name: 'Нежный цыпленок',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A',
+      'assets/images/dataItems/ingredients/chicken.png',
   },
   {
     name: 'Шампиньоны',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324',
+      'assets/images/dataItems/ingredients/shampinioni.png',
   },
   {
     name: 'Ветчина',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61',
+      'assets/images/dataItems/ingredients/vetchina.png',
   },
   {
     name: 'Пикантная пепперони',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3',
+      'assets/images/dataItems/ingredients/cowMeat.png',
   },
   {
     name: 'Острая чоризо',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027',
+      'assets/images/dataItems/ingredients/hot-chizorro.png',
   },
   {
     name: 'Маринованные огурчики',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B',
+      'assets/images/dataItems/ingredients/onio.png',
   },
   {
     name: 'Свежие томаты',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67',
+      'assets/images/dataItems/ingredients/cherry.png',
   },
   {
     name: 'Красный лук',
@@ -93,31 +93,31 @@ export const ingredients = [
     name: 'Сочные ананасы',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0',
+      'assets/images/dataItems/ingredients/ananas.png',
   },
   {
     name: 'Итальянские травы',
     price: 39,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png',
+      'assets/images/dataItems/ingredients/italianTravi.png'
   },
   {
     name: 'Сладкий перец',
     price: 59,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B',
+      'assets/images/dataItems/ingredients/sweetperetc.png',
   },
   {
     name: 'Кубики брынзы',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349',
+      'assets/images/dataItems/ingredients/brinza.png',
   },
   {
     name: 'Митболы',
     price: 79,
     imageUrl:
-      'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png',
+      'assets/images/dataItems/ingredients/meatballs.png',
   },
 ].map((obj, index) => ({ id: index + 1, ...obj }));
 
@@ -125,103 +125,103 @@ export const products = [
   {
     name: 'Омлет с ветчиной и грибами',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
+      'assets/images/dataItems/breakfast/omlet-classic.png',
     categoryId: 2,
   },
   {
     name: 'Омлет с пепперони',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
+      'assets/images/dataItems/breakfast/omlet-pepperoni.png',
     categoryId: 2,
   },
   {
     name: 'Кофе Латте',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+      'assets/images/dataItems/breakfast/omlet-pepperoni.png',
     categoryId: 2,
   },
   {
     name: 'Дэнвич ветчина и сыр',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+      'assets/images/dataItems/breakfast/dosterMeat.png',
     categoryId: 3,
   },
   {
     name: 'Куриные наггетсы',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+      'assets/images/dataItems/breakfast/nagetsi.png',
     categoryId: 3,
   },
   {
     name: 'Картофель из печи с соусом 🌱',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+      'assets/images/dataItems/breakfast/potatoGrill.png',
     categoryId: 3,
   },
   {
     name: 'Додстер',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+      'assets/images/dataItems/breakfast/doster.png',
     categoryId: 3,
   },
   {
     name: 'Острый Додстер 🌶️🌶️',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+      'assets/images/dataItems/breakfast/dosterGrill.png',
     categoryId: 3,
   },
   {
     name: 'Банановый молочный коктейль',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+      'assets/images/dataItems/cofee/shakeMilk.png',
     categoryId: 4,
   },
   {
     name: 'Карамельное яблоко молочный коктейль',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+      'assets/images/dataItems/cofee/shakeStawberry.png',
     categoryId: 4,
   },
   {
     name: 'Молочный коктейль с печеньем Орео',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
+      'assets/images/dataItems/cofee/shakeChockolate.png',
     categoryId: 4,
   },
   {
     name: 'Классический молочный коктейль 👶',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
+      'assets/images/dataItems/cofee/shakeMilk.png',
     categoryId: 4,
   },
   {
     name: 'Ирландский Капучино',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
+      'assets/images/dataItems/cofee/capucchino.png',
     categoryId: 5,
   },
   {
     name: 'Кофе Карамельный капучино',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
+      'assets/images/dataItems/cofee/latecaramel.png',
     categoryId: 5,
   },
   {
     name: 'Кофе Кокосовый латте',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
+      'assets/images/dataItems/cofee/cocoslate.png',
     categoryId: 5,
   },
   {
     name: 'Кофе Американо',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
+      'assets/images/dataItems/cofee/americano.png',
     categoryId: 5,
   },
   {
     name: 'Кофе Латте',
     imageUrl:
-      'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+      'assets/images/dataItems/cofee/late.png',
     categoryId: 5,
   },
 ];
