26c6956b0e3031dc3cfa7a0a169af6c1ea3ff9b0
Artem Lysuk <artemlysuk36@gmail.com>
2026-01-20 21:20:57 +0200

    fixx grid

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -52,7 +52,7 @@ export const ProductsGroupList: React.FC<Props> = ({
 				className="font-extrabold mb-5"
 			/>
 
-			<div className={cn('grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-[50px]', listClassName)}>
+			<div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
 				{items.map((product) => (
 					<ProductCard
 						key={product.id}
