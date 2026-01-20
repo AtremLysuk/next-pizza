b6d2a9b112f188f5d7c78845c4da791a00aa1255
Artem Lysuk <artemlysuk36@gmail.com>
2026-01-20 19:50:04 +0200

    fix grid

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -9,62 +9,62 @@ import {ProductWithRelations} from '@/@types/prisma';
 
 
 interface Props {
-  title: string;
-  items: ProductWithRelations[];
-  categoryId: number;
-  listClassName?: string;
-  className?: string;
+	title: string;
+	items: ProductWithRelations[];
+	categoryId: number;
+	listClassName?: string;
+	className?: string;
 }
 
 export const ProductsGroupList: React.FC<Props> = ({
-                                                     className,
-                                                     title,
-                                                     items,
-                                                     categoryId,
-                                                     listClassName,
+	                                                   className,
+	                                                   title,
+	                                                   items,
+	                                                   categoryId,
+	                                                   listClassName,
                                                    }) => {
-  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+	const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
 
-  const intersectionRef = useRef<HTMLDivElement>(null);
+	const intersectionRef = useRef<HTMLDivElement>(null);
 
-  const intersection = useIntersection(
-    intersectionRef as React.RefObject<HTMLElement>,
-    {
-      threshold: 0.4,
-    }
-  );
+	const intersection = useIntersection(
+		intersectionRef as React.RefObject<HTMLElement>,
+		{
+			threshold: 0.4,
+		}
+	);
 
-  useEffect(() => {
-    if (intersection?.isIntersecting) {
-      setActiveCategoryId(categoryId);
-    }
-  }, [categoryId, intersection?.isIntersecting, title]);
+	useEffect(() => {
+		if (intersection?.isIntersecting) {
+			setActiveCategoryId(categoryId);
+		}
+	}, [categoryId, intersection?.isIntersecting, title]);
 
-  return (
-    <div
-      className={className}
-      id={title}
-      ref={intersectionRef}
-    >
-      <Title
-        text={title}
-        size="lg"
-        className="font-extrabold mb-5"
-      />
+	return (
+		<div
+			className={className}
+			id={title}
+			ref={intersectionRef}
+		>
+			<Title
+				text={title}
+				size="lg"
+				className="font-extrabold mb-5"
+			/>
 
-      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
-        {items.map((product) => (
-          <ProductCard
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            price={product.items[0]?.price ?? 0}
-            count={1}
-            ingredients={product.ingredients}
-            imageUrl={`/${product.imageUrl}`}
-          />
-        ))}
-      </div>
-    </div>
-  );
+			<div className={cn('grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-[50px]', listClassName)}>
+				{items.map((product) => (
+					<ProductCard
+						key={product.id}
+						id={product.id}
+						name={product.name}
+						price={product.items[0]?.price ?? 0}
+						count={1}
+						ingredients={product.ingredients}
+						imageUrl={`/${product.imageUrl}`}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
